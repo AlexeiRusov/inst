b7217833cf46484ec94c3024622962e1ba03dd1d
Student <student@itpark.ru>
2019-09-28 10:39:37 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('worked');
+
+const author = {
+    id: 'busova86',
+    avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
+    verified: true,
+
+
+}
+const photo = {
+    id: 'B23d82NooNF',
+    author: author,
+    location: 'Набережная Амура, Граница с Китааем',
+    imageUrl: 'https://placekitten.com/800/800',
+    videoUrl: null,
+    likes: 159594,
+
+}
+const video = {
+    id: 'B23d82NooNX',
+    author: author,
+    location: 'Торгово-развлекательный центр Острова',
+    imageUrl: 'https://placekitten.com/800/800',
+    videoUrl: 'https://youtube.com/....',
+    likes: 159594,
+}
